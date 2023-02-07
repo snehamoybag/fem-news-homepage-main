4978fe91ed5dc4f5eb155fa34084e662c203c2aa
snehamoybag <snehamoybag2@gmail.com>
2023-02-07 23:49:11 +0530

    do hamburger menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,25 @@
 // STYLESHEET
 import '/scss/style.scss';
+
+// Hamburger Menu
+const navBar = document.querySelector('#nav-bar');
+const navBtn = document.querySelector('#nav-btn');
+let isNavOpen = false;
+
+navBtn.addEventListener('click', () => {
+  (!isNavOpen) ? openNavigation(): closeNavigation();
+});
+
+function openNavigation() {
+  isNavOpen = true;
+  navBtn.setAttribute('aria-expanded', 'true');
+  navBtn.classList.add('open-nav');
+  navBar.classList.add('open-nav');
+}
+
+function closeNavigation() {
+  isNavOpen = false;
+  navBtn.setAttribute('aria-expanded', 'false');
+  navBtn.classList.remove('open-nav');
+  navBar.classList.remove('open-nav');
+}
